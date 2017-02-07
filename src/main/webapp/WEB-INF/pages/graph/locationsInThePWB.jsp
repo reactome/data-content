14ea076a53c43f1ac92da07e4ce17aebb2b7db5f
gviteri <gviteri@ebi.ac.uk>
2017-02-07 15:14:24 +0000

    expand/collapse all.

--- src/main/webapp/WEB-INF/pages/graph/locationsInThePWB.jsp
+++ src/main/webapp/WEB-INF/pages/graph/locationsInThePWB.jsp
@@ -20,37 +20,39 @@
     </legend>
 
 <%--<h5 style="font-size: 17px; margin-left: 23px">Locations in the PathwayBrowser</h5>--%>
+    <div style="text-align: right; padding: 0px 10px 0px 0px;float: right;">
+        <a style="text-align:right; cursor: pointer;" id="pwb_toggle" class="expand">Expand all</a>
+    </div>
 
-
-<div class="wrap">
-    <c:forEach var="topLvl" items="${topLevelNodes}">
-        <c:choose>
-            <c:when test="${empty topLvl.children}">
-                <span style="font-size:13px"><i class="sprite-resize sprite sprite-Pathway" title="${topLvl.type}"></i></span>
-                <a href="${topLvl.url}" <c:if test="${topLvl.highlighted}">class="tree-highlighted-item"</c:if> title="goto Reactome Pathway Browser" rel="nofollow">${topLvl.name} (${topLvl.species})</a>
-            </c:when>
-            <c:otherwise>
-                <%--
-                    The class attribute is used as a jQuery selector. This class is not present in the css.
-                    Specially for chemical, it is present in all species, instead of showing a big list we just show Human as the default
-                    and let the user select the desired species in a dropdown list.
-                 --%>
-                <div class="tplSpe_${fn:replace(topLvl.species, ' ', '_')}" style="display: none">
-                    <span class="plus tree-root" title="click here to expand or collapse the tree">
-                        <i class="sprite-resize-small sprite sprite-plus" title="click here to expand or collapse the tree" style="vertical-align: middle"></i>
-                        <i class="sprite-resize sprite sprite-Pathway" style="vertical-align: middle"></i>
-                        <%--<a href="${topLvl.url}" class="" title="goto Reactome Pathway Browser" rel="nofollow">${topLvl.name} (${topLvl.species})</a>--%>
-                        ${topLvl.name} (${topLvl.species})
-                    </span>
-                    <div class="treeContent">
-                        <ul class="tree">
-                            <c:set var="node" value="${topLvl}" scope="request"/>
-                            <li> <c:import url="node.jsp"/></li>
-                        </ul>
+    <div class="wrap">
+        <c:forEach var="topLvl" items="${topLevelNodes}">
+            <c:choose>
+                <c:when test="${empty topLvl.children}">
+                    <span style="font-size:13px"><i class="sprite-resize sprite sprite-Pathway" title="${topLvl.type}"></i></span>
+                    <a href="${topLvl.url}" <c:if test="${topLvl.highlighted}">class="tree-highlighted-item"</c:if> title="goto Reactome Pathway Browser" rel="nofollow">${topLvl.name} (${topLvl.species})</a>
+                </c:when>
+                <c:otherwise>
+                    <%--
+                        The class attribute is used as a jQuery selector. This class is not present in the css.
+                        Specially for chemical, it is present in all species, instead of showing a big list we just show Human as the default
+                        and let the user select the desired species in a dropdown list.
+                     --%>
+                    <div id="tpla_${topLvl.stId}" class="tplSpe_${fn:replace(topLvl.species, ' ', '_')}" style="display: none">
+                        <span class="plus tree-root" title="click here to expand or collapse the tree">
+                            <i class="sprite-resize-small sprite sprite-plus" title="click here to expand or collapse the tree" style="vertical-align: middle"></i>
+                            <i class="sprite-resize sprite sprite-Pathway" style="vertical-align: middle"></i>
+                            <%--<a href="${topLvl.url}" class="" title="goto Reactome Pathway Browser" rel="nofollow">${topLvl.name} (${topLvl.species})</a>--%>
+                            ${topLvl.name} (${topLvl.species})
+                        </span>
+                        <div class="treeContent">
+                            <ul class="tree">
+                                <c:set var="node" value="${topLvl}" scope="request"/>
+                                <li> <c:import url="node.jsp"/></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </c:otherwise>
-        </c:choose>
-    </c:forEach>
-</div>
+                </c:otherwise>
+            </c:choose>
+        </c:forEach>
+    </div>
 </fieldset>
--- src/main/webapp/resources/js/reactome.search.footer.js
+++ src/main/webapp/resources/js/reactome.search.footer.js
@@ -1,51 +1,54 @@
-$(document).ready(function() {
+var PWB_COOKIE="_PWBTree";
+var EXPAND = "expand";
+var COLLAPSE = "collapse"; // default value for the Location in PWB
+
+$(document).ready(function () {
     $('#local-searchbox').autocomplete({
         serviceUrl: '/content/getTags',
-        minChars:2,
+        minChars: 2,
         deferRequestBy: 250,
         paramName: "tagName",
         delimiter: ",",
-        transformResult: function(response) {
+        transformResult: function (response) {
             return {
-                suggestions: $.map($.parseJSON(response), function(item) {
-                    return { value: item };
+                suggestions: $.map($.parseJSON(response), function (item) {
+                    return {value: item};
                 })
             };
         },
-        onSelect: function(value, data){$("#search_form").submit()}
+        onSelect: function (value, data) {
+            $("#search_form").submit()
+        }
     });
 
-
+    // read cookie when loading page for the first time
+    var pwb_cookie = readCookie(PWB_COOKIE);
+    // PWB Tree always collapse if cookie not set.
+    togglePwbTree(pwb_cookie == null ? COLLAPSE : pwb_cookie);
 });
 
-$('ul.term-list').each(function(){
-
+$('ul.term-list').each(function () {
     var LiN = $(this).find('li').length;
-
-    if( LiN > 6){
+    if (LiN > 6) {
         $('li', this).eq(5).nextAll().hide().addClass('toggleable');
         $(this).append('<li class="more">More...</li>');
     }
-
 });
 
 
-$('ul.term-list').on('click','.more', function(){
-
-    if( $(this).hasClass('less') ){
+$('ul.term-list').on('click', '.more', function () {
+    if ($(this).hasClass('less')) {
         $(this).text('More...').removeClass('less');
-    }else{
+    } else {
         $(this).text('Less...').addClass('less');
     }
-
     $(this).siblings('li.toggleable').slideToggle();
-
 });
 
-$('#search_form').submit(function(e) {
+$('#search_form').submit(function (e) {
     if (!$('#local-searchbox').val()) {
         e.preventDefault();
-    } else if ($('#local-searchbox').val().match(/^\s*$/)){
+    } else if ($('#local-searchbox').val().match(/^\s*$/)) {
         e.preventDefault();
     }
 });
@@ -54,7 +57,7 @@ $(".plus").click(function () {
     $plus = $(this);
     $treeContent = $plus.nextAll().eq(0);
     $treeContent.slideToggle(500, function () {
-        if ($treeContent.is(":visible") ){
+        if ($treeContent.is(":visible")) {
             return $plus.find(".sprite-plus").attr("class", "sprite-resize-small sprite sprite-minus");
         } else {
             return $plus.find(".sprite-minus").attr("class", "sprite-resize-small sprite sprite-plus");
@@ -62,13 +65,13 @@ $(".plus").click(function () {
     });
 });
 
-$('#availableSpeciesSel').ready(function() {
+$('#availableSpeciesSel').ready(function () {
     var DEFAULT_SPECIES = 'Homo sapiens';
 
     /** Check if hash is present in the URL **/
     var hash = decodeURIComponent(window.location.hash);
     var defaulLoaded = false;
-    if(hash == "") {
+    if (hash == "") {
         $("div[class*=tplSpe_]").each(function (index, value) {
             var item = $(value).attr("class");
             if (item == "tplSpe_" + DEFAULT_SPECIES.replace(" ", "_")) {
@@ -76,20 +79,20 @@ $('#availableSpeciesSel').ready(function() {
                 $("." + item).show();
 
                 //change url
-                if($("#availableSpeciesSel").val() != null) {
+                if ($("#availableSpeciesSel").val() != null) {
                     window.location.hash = "#" + encodeURIComponent(DEFAULT_SPECIES);
                 }
 
                 defaulLoaded = true;
-            }else {
+            } else {
                 $("." + item).css("display", "none");
             }
         });
 
-        if(!defaulLoaded){
+        if (!defaulLoaded) {
             $("div[class*=tplSpe_]").css("display", "block");
         }
-    }else {
+    } else {
         hash = hash.replace("#", "").replace(" ", "_");
 
         // hash has been change manually into a non-existing value. Pick the first one which is human
@@ -112,11 +115,11 @@ $('#availableSpeciesSel').ready(function() {
     }
 });
 
-$('#availableSpeciesSel').on('change', function() {
+$('#availableSpeciesSel').on('change', function () {
     var selectedSpecies = this.value;
 
     // hide everything
-    $("div[class*=tplSpe_]").each(function( index, element ){
+    $("div[class*=tplSpe_]").each(function (index, element) {
         $(element).hide();
     });
 
@@ -124,6 +127,45 @@ $('#availableSpeciesSel').on('change', function() {
     $(".tplSpe_" + selectedSpecies).show();
 
     // change anchor in the URL
-    window.location.hash = "#"+encodeURIComponent(selectedSpecies.replace("_", " "));
+    window.location.hash = "#" + encodeURIComponent(selectedSpecies.replace("_", " "));
 
 });
+
+// expand and collapse all feature
+$("#pwb_toggle").click(function () {
+    var action = $("#pwb_toggle").attr("class");
+    togglePwbTree(action);
+    document.cookie = PWB_COOKIE + "=" + action; // set the cookie
+});
+
+function togglePwbTree(action){
+    var treeContent = $("div.treeContent");
+    treeContent.each(function (index, element) {
+        if (action == EXPAND) {
+            $(element).show();
+        } else {
+            $(element).hide(100);
+        }
+    });
+
+    if (action == COLLAPSE) {
+        $(".sprite-minus").attr("class", "sprite-resize-small sprite sprite-plus");
+        $("#pwb_toggle").text("Expand All");
+        $("#pwb_toggle").attr("class", EXPAND);
+    } else {
+        $(".sprite-plus").attr("class", "sprite-resize-small sprite sprite-minus");
+        $("#pwb_toggle").text("Collapse All");
+        $("#pwb_toggle").attr("class", COLLAPSE);
+    }
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
