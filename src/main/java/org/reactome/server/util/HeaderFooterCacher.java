e6ac0c108381095a6900a6880b006c54493ee587
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-10-23 14:50:57 +0100

    HeaderFooterCacher management updated

--- src/main/java/org/reactome/server/util/HeaderFooterCacher.java
+++ src/main/java/org/reactome/server/util/HeaderFooterCacher.java
@@ -3,9 +3,6 @@ package org.reactome.server.util;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -14,7 +11,7 @@ import java.net.URL;
 /**
  * Generates the header and the footer every MINUTES defined below.
  * The header.jsp and footer.jsp are placed under jsp folder in WEB-INF
- *
+ * <p>
  * IMPORTANT
  * ---------
  * We assume the war file runs exploded, because there is no way of writing
@@ -24,9 +21,8 @@ import java.net.URL;
  *
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
-@Component
 public class HeaderFooterCacher extends Thread {
-    private static final Logger logger = LoggerFactory.getLogger("templateLogger");
+    private static final Logger logger = LoggerFactory.getLogger("threadLogger");
 
     private static final String TITLE_OPEM = "<title>";
     private static final String TITLE_CLOSE = "</title>";
@@ -43,43 +39,44 @@ public class HeaderFooterCacher extends Thread {
 
     private final String server;
 
-    @Autowired
-    public HeaderFooterCacher(@Value("${template.server}") String server) {
+    private boolean active = true;
+
+    public HeaderFooterCacher(String server) {
+        super("DC-HeaderFooter");
         this.server = server;
         start();
     }
 
     @Override
     public void run() {
-        //noinspection InfiniteLoopStatement
-        while (isAlive()) {
-            writeFile("header.jsp", getHeader());
-            writeFile("footer.jsp", getFooter());
-            try {
-                Thread.sleep(1000 * 60 * MINUTES);
-            } catch (InterruptedException e) {
-                logger.warn("The header/footer updater has been stop for the data-content");
-                interrupt();
+        try {
+            while (active) {
+                writeFile("header.jsp", getHeader());
+                writeFile("footer.jsp", getFooter());
+                if (active) Thread.sleep(1000 * 60 * MINUTES);
+
             }
+        } catch (InterruptedException e) {
+            logger.info("Data-Content HeaderFooterCacher interrupted");
         }
     }
 
-    private synchronized void writeFile(String fileName, String content){
+    private synchronized void writeFile(String fileName, String content) {
         try {
             //noinspection ConstantConditions
             String path = getClass().getClassLoader().getResource("").getPath();
             //HACK!
-            if(path.contains("WEB-INF")) {
+            if (path.contains("WEB-INF")) {
                 //When executing in a deployed war file in tomcat, the WEB-INF folder is just one bellow the classes
                 path += "../pages/";
-            }else{
+            } else {
                 //When executing in local we need to write the files in the actual resources
                 path += "../../src/main/webapp/WEB-INF/pages/";
             }
             FileOutputStream out = new FileOutputStream(path + fileName);
             out.write(content.getBytes());
             out.close();
-        } catch (NullPointerException | IOException e){
+        } catch (NullPointerException | IOException e) {
             logger.error(e.getMessage());
             interrupt();
         }
@@ -93,19 +90,19 @@ public class HeaderFooterCacher extends Thread {
             rtn = getReplaced(rtn, SEARCH_OPEN, SEARCH_CLOSE, SEARCH_REPLACE);
             rtn = getReplaced(rtn, HEADER_CLOSE, HEADER_CLOSE, HEADER_CLOSE_REPLACE);
             rtn = rtn.replaceAll("(http|https)://", "//");
-            return  rtn;
+            return rtn;
         } catch (IOException e) {
             logger.error(e.getMessage());
             return String.format("<span style='color:red'>%s</span>", e.getMessage());
         }
     }
 
-    private String getReplaced(String target, String open, String close, String replace){
+    private String getReplaced(String target, String open, String close, String replace) {
         try {
             String pre = target.substring(0, target.indexOf(open));
             String suf = target.substring(target.indexOf(close) + close.length(), target.length());
             return pre + replace + suf;
-        }catch (StringIndexOutOfBoundsException e){
+        } catch (StringIndexOutOfBoundsException e) {
             return target;
         }
     }
@@ -122,4 +119,10 @@ public class HeaderFooterCacher extends Thread {
         }
     }
 
+    @Override
+    public void interrupt() {
+        active = false;
+        super.interrupt();
+        logger.info("Data-Content HeaderFooterCacher stopped");
+    }
 }
