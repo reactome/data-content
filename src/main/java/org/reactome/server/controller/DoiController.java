c494db035df0d8d7759797dff71f13fea6aebade
cgong <cgong@ebi.ac.uk>
2020-05-08 16:33:46 +0100

    add another http request to get toc pathways and custom query

--- src/main/java/org/reactome/server/controller/DoiController.java
+++ src/main/java/org/reactome/server/controller/DoiController.java
@@ -3,7 +3,7 @@ package org.reactome.server.controller;
 
 import org.reactome.server.graph.exception.CustomQueryException;
 import org.reactome.server.graph.service.*;
-import org.reactome.server.result.CustomPathway;
+import org.reactome.server.result.PathwayResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +23,7 @@ class DoiController {
 
     private static final String TITLE = "title";
     private static final String DOIPATHWAYS = "doiPathways";
+    private static final String TOCPATHWAYS = "tocPathways";
 
     private AdvancedDatabaseObjectService advancedDatabaseObjectService;
 
@@ -39,19 +40,29 @@ class DoiController {
     @RequestMapping(value = "/doi", method = RequestMethod.GET)
     public String doiTable(ModelMap model) {
 
-        Collection<CustomPathway> doiPathways = getAllDoiPathways();
+        Collection<PathwayResult> doiPathways = getAllDoiPathways();
 
         model.addAttribute(TITLE, "DOI Table");
         model.addAttribute(DOIPATHWAYS, doiPathways);
         return "graph/doi";
     }
 
+
+    @RequestMapping(value = "/toc", method = RequestMethod.GET)
+    public String tocTable(ModelMap model) {
+
+        Collection<PathwayResult> tocPathways = getAllTocPathways();
+        model.addAttribute(TITLE, "Table of Contens");
+        model.addAttribute(TOCPATHWAYS, tocPathways);
+        return "graph/toc";
+    }
+
     /**
-     * Retrieve all pathway which the doi value is not null, and all the authors,reviewers, editors are
-     * related  to this pathway, subpathway and reactions.
+     * Retrieve all pathways which the doi value is not null, and all the authors, reviewers, editors are
+     * related to this pathway, sub pathway and reactions.
      */
-    public Collection<CustomPathway> getAllDoiPathways() {
-        Collection<CustomPathway> rtn;
+    public Collection<PathwayResult> getAllDoiPathways() {
+        Collection<PathwayResult> rtn;
 
         String query = "MATCH (p:Pathway)" +
                 "WHERE EXISTS(p.doi)" +
@@ -62,18 +73,64 @@ class DoiController {
                 "OPTIONAL MATCH (rle)<-[:reviewed]-(:InstanceEdit)<-[:author]-(rler:Person)" +
                 "OPTIONAL MATCH (rle)<-[:edited]-(:InstanceEdit)<-[:author]-(rlee:Person)" +
                 "RETURN p.displayName AS displayName, " +
-                "p.doi as doi, " +
+                "p.doi AS doi, " +
+                "p.stId AS stId, " +
                 "p.speciesName AS species, " +
-                "collect(distinct rlea) as authors, " +
-                "p.releaseDate As releaseDate, " +
-                "max(re.dateTime) As reviseDate, " +
-                "collect(distinct rler) AS reviewers," +
-                "collect(distinct rlee) AS editors," +
-                "p.releaseStatus AS releaseStatus " +
+                "p.releaseDate AS releaseDate, " +
+                "p.releaseStatus AS releaseStatus, " +
+                "max(re.dateTime) AS reviseDate, " +
+                "collect(distinct rlea) AS authors, " +
+                "collect(distinct rler) AS reviewers, " +
+                "collect(distinct rlee) AS editors " +
                 "ORDER BY toLower(p.displayName)";
 
         try {
-            rtn = advancedDatabaseObjectService.getCustomQueryResults(CustomPathway.class, query);
+            rtn = advancedDatabaseObjectService.getCustomQueryResults(PathwayResult.class, query);
+        } catch (CustomQueryException e) {
+            errorLogger.error(e.getMessage(), e);
+            rtn = new ArrayList<>();
+        }
+        return rtn;
+    }
+
+    /**
+     * Retrieve all top level pathways and child pathways,  all the authors,reviewers, editors are
+     * related  to this pathway, sub pathway and reactions.
+     */
+    public Collection<PathwayResult> getAllTocPathways() {
+        Collection<PathwayResult> rtn;
+
+        String query = "MATCH (p:TopLevelPathway)" +
+                "OPTIONAL MATCH (p)<-[:revised]-(re:InstanceEdit)" +
+                "OPTIONAL MATCH (p)-[:reviewed]-(:InstanceEdit)" +
+                "OPTIONAL MATCH (p)-[:hasEvent*]->(rle:ReactionLikeEvent)" +
+                "OPTIONAL MATCH (p)-[:hasEvent]->(pa:Pathway{hasDiagram:True})" +
+                "OPTIONAL MATCH (rle)<-[:authored]-(:InstanceEdit)<-[:author]-(rlea:Person)" +
+                "OPTIONAL MATCH (rle)<-[:reviewed]-(:InstanceEdit)<-[:author]-(rler:Person)" +
+                "OPTIONAL MATCH (rle)<-[:edited]-(:InstanceEdit)<-[:author]-(rlee:Person)" +
+                "WITH  size(collect(distinct rlea.displayName)) AS authorsCollect, " +
+                "p, " +
+                "collect(distinct rlea) AS authors, " +
+                "max(re.dateTime) AS revised, " +
+                "collect(distinct rler) AS reviewers, " +
+                "collect(distinct rlee) AS editors, " +
+                "collect(distinct pa) AS subPathway " +
+                "WHERE authorsCollect > 0 " +
+                "RETURN p.displayName AS displayName, " +
+                "p.doi AS doi, " +
+                "p.releaseDate AS releaseDate, " +
+                "p.releaseStatus AS releaseStatus, " +
+                "p.speciesName AS species, " +
+                "p.stId AS stId, " +
+                "authors AS authors, " +
+                "reviewers AS reviewers, " +
+                "subPathway AS subPathway, " +
+                "editors AS editors, " +
+                "revised AS reviseDate " +
+                "ORDER BY toLower(p.displayName)";
+
+        try {
+            rtn = advancedDatabaseObjectService.getCustomQueryResults(PathwayResult.class, query);
         } catch (CustomQueryException e) {
             errorLogger.error(e.getMessage(), e);
             rtn = new ArrayList<>();
