687610d06c6a94533675760e4f65f1d64c29a1fe
cgong <cgong@ebi.ac.uk>
2020-05-14 17:50:14 +0100

    call the service from graph core

--- src/main/java/org/reactome/server/controller/DoiController.java
+++ src/main/java/org/reactome/server/controller/DoiController.java
@@ -1,8 +1,9 @@
 package org.reactome.server.controller;
 
 
-import org.reactome.server.result.PathwayResult;
-import org.reactome.server.util.TocDoiService;
+import org.reactome.server.graph.domain.result.PathwayResult;
+import org.reactome.server.graph.service.DoiService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,48 +12,35 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.*;
+import java.util.Collection;
 
 
 @SuppressWarnings("unused")
 @Controller
-class TocDoiController {
+class DoiController {
 
     private static final Logger errorLogger = LoggerFactory.getLogger("errorLogger");
 
     private static final String TITLE = "title";
     private static final String DOIPATHWAYS = "doiPathways";
-    private static final String TOCPATHWAYS = "tocPathways";
 
-
-    private TocDoiService tocDoiService;
+    private DoiService doiService;
 
     @Autowired
-    public void setTocDoiService(TocDoiService tocDoiService) {
-        this.tocDoiService = tocDoiService;
+    public void setDoiService(DoiService doiService) {
+        this.doiService = doiService;
     }
 
-    public TocDoiController() {
+    public DoiController() {
     }
 
     @RequestMapping(value = "/doi", method = RequestMethod.GET)
     public String doiTable(ModelMap model) {
 
-        Collection<PathwayResult> doiPathways = tocDoiService.getAllDoiPathways();
+        Collection<PathwayResult> doiPathways = doiService.getAllDoiPathway();
 
         model.addAttribute(TITLE, "DOI Table");
         model.addAttribute(DOIPATHWAYS, doiPathways);
         return "graph/doi";
-    }
-
-
-    @RequestMapping(value = "/toc", method = RequestMethod.GET)
-    public String tocTable(ModelMap model) {
-
-        Collection<PathwayResult> tocPathways = tocDoiService.getAllTocPathways();
-
-        model.addAttribute(TITLE, "Table of Contents");
-        model.addAttribute(TOCPATHWAYS, tocPathways);
-        return "graph/toc";
     }
 }
