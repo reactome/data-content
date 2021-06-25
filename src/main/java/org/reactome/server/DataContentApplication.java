6b84c2c93a8bc4c6f2cc845623c762b625a9b0d9
EliotRagueneau <159753.AZErty>
2021-06-25 17:10:21 +0100

    :arrow_up: Upgrade to latest graph core usage

--- src/main/java/org/reactome/server/DataContentApplication.java
+++ src/main/java/org/reactome/server/DataContentApplication.java
@@ -1,13 +1,10 @@
 package org.reactome.server;
 
-import org.aspectj.lang.Aspects;
-import org.reactome.server.graph.aop.LazyFetchAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"org.reactome.server"})
 @EntityScan("org.reactome.server.graph.domain.model")
@@ -20,14 +17,5 @@ public class DataContentApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DataContentApplication.class, args);
-    }
-
-    /**
-     * This is needed to get hold of the instance of the aspect which is created outside of the spring container,
-     * and make it available for autowiring.
-     */
-    @Bean
-    LazyFetchAspect lazyFetchAspect() {
-        return Aspects.aspectOf(LazyFetchAspect.class);
     }
 }
